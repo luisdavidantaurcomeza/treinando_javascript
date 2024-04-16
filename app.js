92d29c9f23994b7b060435089c09a34408c4631e
luis david <luis.meza@escola.pr.gov.br>
2024-04-16 16:45:51 -0300

    utilizando o console para depurar msgs, if e else, bloco de codigo, tamplate string para concatenar o numero secreto com o valor armazenado

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 29;
+
+// aqui está errado, eu preciso alterar a informação do console pois a pessoa pode ver o numero pelo inspetor
+console.log(numeroSecreto);
 let chute = prompt ('Escolha um número entre um e trinta: ');
 
-if (numeroSecreto == chute) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
 
+// se o chute for igual ao numero secreto quero executar algo
+if (numeroSecreto == chute) {
+    alert(`Isso ai! Você descobriu o número secreto, que é ${numeroSecreto}`);
+
+} else {
+    alert ('Você errou :(');
 }
+
+
+
