8b84ef7a63a0dcea798fd28a8018deb2beea2a96
luis david <luisdavid_204@hotmail.com>
2024-04-20 14:14:26 -0300

    math e parseint e sobre operadores ternarios

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 29;
+let numeroSecreto = parseInt(Math.random()*10+1);
 
 // aqui está errado, eu preciso alterar a informação do console pois a pessoa pode ver o numero pelo inspetor
 console.log(numeroSecreto);
@@ -11,10 +11,10 @@ let tentativas=1;
 
 
 while (chute!=numeroSecreto) {
-        chute = prompt ('Escolha um número entre um e trinta: ');
+        chute = prompt ('Escolha um número 1 um e 10: ');
        
     // se o chute for igual ao numero secreto quero executar algo
-    if (numeroSecreto == chute) {
+    if (chute == numeroSecreto) {
         //alert(`Isso ai! Você descobriu o número secreto, que é ${numeroSecreto} com ${tentativas} tentativas`);
         //quero que o while não se repita mais, ou seja, pare.
         break;
@@ -27,7 +27,7 @@ while (chute!=numeroSecreto) {
 
         }
 
-        //várias tentativas
+        //tentativas = tentativas+1
         tentativas++;
 
     
@@ -35,19 +35,15 @@ while (chute!=numeroSecreto) {
     // coloquei fora do while:
     //agora vou fazer um if else para melhorar, colocar no singular e no plural 
     //criar uma variavel com o operador ternario
-    
-    let palavraTentativa = tentativa > 1 ? 'tentativas':'tentativa';
-    alert(`Isso ai! Você descobriu o número secreto, que é ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
-
-    
-    
-    /* if(tentativas>1){
+      
+          
+}
+/* if(tentativas>1){
     alert(`Isso ai! Você descobriu o número secreto, que é ${numeroSecreto} com ${tentativas} tentativas`);
     } else{
         alert(`Isso ai! Você descobriu o número secreto, que é ${numeroSecreto} com ${tentativas} tentativa`);
     }
  */
-    
-}
-
+let palavraTentativa = tentativa > 1 ? 'tentativas':'tentativa';
+    alert(`Isso ai! Você descobriu o número secreto, que é ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
 
